1cb1798c86f21b28e5c60eaa94a164cfc7b08cf4
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-09-19 19:25:20 -0300

    Added a video parser

---
+++ src/main/java/io/github/seujorgenochurras/video/buffer/PixelAnalyzerBuffer.java
@@ -0,0 +1,46 @@
+package io.github.seujorgenochurras.video.buffer;
+
+import io.github.seujorgenochurras.image.BetterImage;
+import io.metaloom.video4j.VideoFile;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PixelAnalyzerBuffer {
+
+    private final VideoFile videoFile;
+
+    private final int maxBufferedPixels;
+
+    public PixelAnalyzerBuffer(VideoFile videoFile) {
+        this.videoFile = videoFile;
+        this.maxBufferedPixels = 250;
+    }
+
+    public PixelAnalyzerBuffer(VideoFile videoFile, int maxBufferedPixels) {
+        this.videoFile = videoFile;
+        this.maxBufferedPixels = maxBufferedPixels;
+    }
+
+    private final Queue<BetterImage> pixelGroupQueue = new LinkedBlockingQueue<>();
+    public void startQueue() {
+        new Thread(() -> {
+            while (videoFile.currentFrame() < videoFile.length()) {
+                if(pixelGroupQueue.size() >= maxBufferedPixels){
+                    continue;
+                }
+                BetterImage betterImage = new BetterImage(videoFile.frameToImage());
+                pixelGroupQueue.offer(betterImage);
+            }
+        }).start();
+    }
+
+    public Queue<BetterImage> getQueue() {
+        return pixelGroupQueue;
+    }
+
+    public long getVideoFileLength(){
+        return videoFile.length();
+    }
+
+}
