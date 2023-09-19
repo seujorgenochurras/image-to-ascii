1ecaccde4a2b4885e43292080126fa8ee143c17e
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-09-19 19:25:20 -0300

    Added a video parser

--- src/main/java/io/github/seujorgenochurras/image/pixel/ImagePixelGroup.java
+++ src/main/java/io/github/seujorgenochurras/image/pixel/ImagePixelGroup.java
@@ -1,7 +1,6 @@
 package io.github.seujorgenochurras.image.pixel;
 
 import java.util.ArrayList;
-import java.util.function.Consumer;
 
 public class ImagePixelGroup extends ArrayList<ImagePixel> {
 
--- src/test/java/io/github/seujorgenochurras/color/FindBestSymbolPatternTest.java
+++ src/test/java/io/github/seujorgenochurras/color/FindBestSymbolPatternTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import static io.github.seujorgenochurras.util.StringUtils.getUTFChars;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FindBestSymbolPatternTest {
+class FindBestSymbolPatternTest {
     @Test
     void isFindingBestPattern(){
         var symbols = BestSymbolPatternFinder.findBestPattern(10, 110, getUTFChars(32, 126));
-        System.out.println(symbols);
-
+        String[] expectedSymbols = {" ", "`", "'", ",", ":", "^", "*", "+", "-", ".", "r", ")", "/", "a", "E", "p", "G", "0", "1", "2", "3", "9", "@", "A", "B", "D", "H", "M", "W"};
+        assertEquals(expectedSymbols, symbols.getSymbolsAsStringArray());
     }
 }
