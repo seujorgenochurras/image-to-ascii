30a76d0fc7814e03d7cbe9b04f60cc27df92ae78
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-02-19 17:40:45 -0300

    Add test

---
+++ src/test/java/io/github/seujorgenochurras/image/ascii/AsciiParserTest.java
@@ -0,0 +1,7 @@
+package io.github.seujorgenochurras.image.ascii;
+
+public class AsciiParserTest {
+
+
+
+}
---
+++ src/test/java/io/github/seujorgenochurras/image/bright/AlgorithmsTest.java
@@ -0,0 +1,54 @@
+package io.github.seujorgenochurras.image.bright;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import io.github.seujorgenochurras.image.ascii.algorithm.pixel.bright.BrightestAlgorithm;
+import io.github.seujorgenochurras.image.ascii.algorithm.pixel.bright.DarkestPixelAlgorithm;
+import io.github.seujorgenochurras.image.ascii.algorithm.pixel.bright.HumanEyeAlgorithm;
+import io.github.seujorgenochurras.image.ascii.algorithm.pixel.bright.LightestAlgorithm;
+import org.junit.jupiter.api.Test;
+
+public class AlgorithmsTest {
+
+    private static final HumanEyeAlgorithm humanEyeAlgorithm = new HumanEyeAlgorithm();
+    private static final BrightestAlgorithm brightestAlgorithm = new BrightestAlgorithm();
+    private static final DarkestPixelAlgorithm darkestPixelAlgorithm = new DarkestPixelAlgorithm();
+    private static final LightestAlgorithm lightestAlgorithm = new LightestAlgorithm();
+
+    @Test
+    void givenColor_whenIsDark_thenReturnLowBrightnessValue(){
+        int red = 3;
+        int green = 12;
+        int blue = 21;
+
+        int humanEye = humanEyeAlgorithm.getPixelRepresentation(red, green, blue);
+        int brightestAlgo = brightestAlgorithm.getPixelRepresentation(red, green, blue);
+        int darkest = darkestPixelAlgorithm.getPixelRepresentation(red, green, blue);
+        int lightest = lightestAlgorithm.getPixelRepresentation(red, green, blue);
+
+        assertTrue(humanEye < 50);
+        assertTrue(brightestAlgo < 50);
+        assertTrue(darkest < 50);
+        assertTrue(lightest < 50);
+
+    }
+
+    @Test
+    void givenColor_whenIsWhite_thenReturnHighBrightnessValue(){
+        int red = 200;
+        int green = 122;
+        int blue = 221;
+
+        int humanEye = humanEyeAlgorithm.getPixelRepresentation(red, green, blue);
+        int brightestAlgo = brightestAlgorithm.getPixelRepresentation(red, green, blue);
+        int darkest = darkestPixelAlgorithm.getPixelRepresentation(red, green, blue);
+        int lightest = lightestAlgorithm.getPixelRepresentation(red, green, blue);
+
+        assertTrue(humanEye > 70);
+        assertTrue(brightestAlgo > 70);
+        assertTrue(darkest > 70);
+        assertTrue(lightest > 70);
+
+    }
+}
