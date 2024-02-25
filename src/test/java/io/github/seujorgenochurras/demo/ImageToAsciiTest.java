fb45273740231f4002a545ff9ade896a3f14944a
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-02-25 17:42:06 -0300

    chore: Removed unused Opencv and Video4j dependencies

---
+++ src/test/java/io/github/seujorgenochurras/demo/ImageToAsciiTest.java
@@ -0,0 +1,2 @@
+package io.github.seujorgenochurras.demo;public class ImageToAsciiTest {
+}
---
+++ src/test/java/io/github/seujorgenochurras/validator/ParserBuilderValidatorTest.java
@@ -0,0 +1,17 @@
+package io.github.seujorgenochurras.validator;
+
+import io.github.seujorgenochurras.image.ascii.ParserBuilder;
+import io.github.seujorgenochurras.image.ascii.exception.NoSymbolException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserBuilderValidatorTest {
+
+    @Test
+    void givenValidator_whenHasNoSymbols_thenThrowNoSymbolsException(){
+        ParserBuilderValidator validator = new ParserBuilderValidator();
+
+        assertThrows(NoSymbolException.class, () -> validator.validateSymbols(new String[0]));
+    }
+}
